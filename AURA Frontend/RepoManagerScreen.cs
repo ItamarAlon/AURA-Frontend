c1728b27065f02d9c22940316933a86dd3f40f9b
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2025-05-16 16:50:11 +0300

    Created empty RepoManagerScreen

---
+++ AURA Frontend/RepoManagerScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AURA_Frontend
+{
+    public partial class RepoManagerScreen : UserControl
+    {
+        public RepoManagerScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
